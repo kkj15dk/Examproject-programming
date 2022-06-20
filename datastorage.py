c8c8cd075ff5d7f232f41f60d275aa2c37bae652
kkj15dk <kasperkrunderup@gmail.com>
2022-06-20 10:17:40 +0200

    short comments for datastorage

--- datastorage.py
+++ datastorage.py
@@ -1,6 +1,7 @@
 import pickle
 
 class system():
+    # A class for saving the cyrrent system using pickle
     def __init__(self, name, lettermap, start, scaling):
         self.name = name
         self.lettermap = lettermap
@@ -8,6 +9,8 @@ class system():
         self.scaling = scaling
 
 def loadall(filename):
+    # A function for loading all systems saved in the systems.dat.
+    # A generator
     with open(filename, "rb") as f:
         while True:
             try:
